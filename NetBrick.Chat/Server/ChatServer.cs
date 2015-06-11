a03cd65b2362b98526d7939ad2a6928c69ecfe11
MicleBrick <miclebrick@outlook.com>
2015-06-10 22:50:22 -0400

    Modified Lidgren Properties

--- NetBrick.Chat/Server/ChatServer.cs
+++ NetBrick.Chat/Server/ChatServer.cs
@@ -11,7 +11,6 @@ namespace NetBrick.Chat.Server
     {
         public abstract string MasterAddress { get; }
         public abstract int MasterPort { get; }
-        public abstract List<IPEndPoint> Servers { get; }
 
         public ChatServer(string appIdentifier, int port, List<IPEndPoint> servers, int maxConnections = 10, string address = "127.0.0.1")
             : base(appIdentifier, port, maxConnections, address)

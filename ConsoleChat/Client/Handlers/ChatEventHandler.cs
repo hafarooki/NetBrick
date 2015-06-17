25e46d20256fbc39a79e72665e24f09bd98cc690
MicleBrick <miclebrick@outlook.com>
2015-06-17 19:37:40 -0400

    Fixed some syncing issues.

---
+++ ConsoleChat/Client/Handlers/ChatEventHandler.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using ConsoleChat.Codes;
+using NetBrick.Core;
+using NetBrick.Core.Client.Handlers;
+
+namespace ConsoleChat.Client.Handlers
+{
+    public class ChatEventHandler : PacketHandler
+    {
+        public override short Code => (short)ChatEventCode.Chat;
+        public override PacketType Type => PacketType.Event;
+
+        public override void Handle(Packet packet)
+        {
+            Console.WriteLine((string)packet.Parameters[(byte)ChatParameterCode.Message]);
+        }
+    }
+}
